05a59a5366ae1ea75dbbafa09e17fbc06f7a3faa
ChristinaBuramenskaya <110132519+ChristinaBuramenskaya@users.noreply.github.com>
2022-12-18 16:36:47 +0300

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,13 @@
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+
+f = open('file01.txt', 'a', encoding = 'utf_8')
+data = ''
+f.write(data)
+f.close()
+
+f = open('file01.txt', 'r', encoding = 'utf_8')
+str = f.read().split()
+print(str)
+new_str = [n for n in str if 'абв' not in n]
+print(new_str)
+f.close()
